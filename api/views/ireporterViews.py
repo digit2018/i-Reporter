69798a46780509fbdaaf2aa83a67ba8040a30be3
digit2018 <arochaedward2018@gmail.com>
2019-01-04 08:27:18 -0800

    [finishes #162265332] create get incident api

---
+++ api/views/ireporterViews.py
@@ -0,0 +1,33 @@
+from flask import Blueprint
+from api.controllers.ireportercontrollers import addUser, addIncident, getAllIncidents
+from python import pytest
+
+bp = Blueprint("ireporterViews", __name__, url_prefix="/api/v1")
+
+@bp.route("/users", methods=["POST"])
+def createUser():
+    return addUser()
+
+@bp.route("/red-flags", methods=["POST"])
+def createIncident():
+    return addIncident()
+
+@bp.route("/red-flags", methods=["GET"])
+def get_all_red_flag_records():
+    return getAllIncidents()
+
+@bp.route("/red-flags/<int:red_flag_id>", methods=["GET"])
+def get_specific_redflag():
+    pass
+
+@bp.route("/red-flags/<int:red_flag_id>/location", methods=["PATCH"])
+def edit_specific_redflag_location():
+    pass
+
+@bp.route("/red-flags/<int:red_flag_id>/comment", methods=["PATCH"])
+def add_comment_to_specific_redflag_record():
+    pass
+
+@bp.route("/red-flags/<int:red_flag_id>", methods=["DELETE"])
+def delete_specific_redflag_record():
+    pass
