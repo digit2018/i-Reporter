6703c176a49d603954cb30179f4c17b46e1b4f0a
digit2018 <arochaedward2018@gmail.com>
2019-01-04 07:50:54 -0800

    [finishes #162265341] create delete incident api

---
+++ api/views/ireporterViews.py
@@ -0,0 +1,32 @@
+from flask import Blueprint
+from api.controllers.ireportercontrollers import addUser, addIncident, getAllIncidents, searchId, incidents, deleteId
+from python import pytest
+bp = Blueprint("ireporterViews", __name__, url_prefix="/api/v1")
+
+@bp.route("/users", methods=["POST"])
+def createUser():
+    return addUser()
+
+@bp.route("/red-flags", methods=["POST"])
+def createIncident():
+    return addIncident()
+
+@bp.route("/red-flags", methods=["GET"])
+def get_all_red_flag_records():
+    return getAllIncidents()
+
+@bp.route("/red-flags/<int:red_flag_id>", methods=["GET"])
+def get_specific_redflag(red_flag_id):
+    return searchId(red_flag_id, incidents)
+
+@bp.route("/red-flags/<int:red_flag_id>/location", methods=["PATCH"])
+def edit_specific_redflag_location():
+    pass
+
+@bp.route("/red-flags/<int:red_flag_id>/comment", methods=["PATCH"])
+def add_comment_to_specific_redflag_record():
+    pass
+
+@bp.route("/red-flags/<int:red_flag_id>", methods=["DELETE"])
+def delete_specific_redflag_record(red_flag_id):
+    return deleteId(red_flag_id, incidents)
